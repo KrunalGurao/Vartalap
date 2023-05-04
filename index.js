2c83812b0aeb189c5cfc0a7921f01b827a2c7f7a
KrunalGurao <krunalgurao1@gmail.com>
2023-05-04 20:35:02 +0530

    Day-1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { connection } = require('./configs/db');
 const { adminRouter } = require('./routes/admin.routes');
-const userRouter = require('./routes/user');
+const {userRouter} = require('./routes/user');
 require('dotenv').config();
 
 const app = express();
