42e096b13d99b6a9bab038da8c42ebb90cd571d0
Rutujadd27 <rutujadd27@gmail.com>
2023-05-04 23:24:16 +0530

    day-1 private chat

--- configs/db.js
+++ configs/db.js
@@ -1,13 +1,19 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const connection = async () =>{
-    try{
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log('Connection with MongoDB established');
-    }catch(err){
-        console.log('Something went wrong with Mongdb Connection');
-    }
-}
-module.exports = {
-    connection
-}
+// const connection = async () =>{
+//     try{
+//         await mongoose.connect(process.env.MONGODB_URI);
+//         console.log('Connection with MongoDB established');
+//     }catch(err){
+//         console.log('Something went wrong with Mongdb Connection');
+//     }
+// }
+// module.exports = {
+//     connection
+// }
+
+const mongoose = require("mongoose")
+require("dotenv").config()
+const connection = mongoose.connect(process.env.MONGODB_URI)
+
+module.exports={connection}
