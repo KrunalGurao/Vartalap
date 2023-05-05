a93d2733a395b388fdd5dbeeb0d3b59e750c68f1
KrunalGurao <krunalgurao1@gmail.com>
2023-05-05 23:13:22 +0530

    Day-2

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { connection } = require('./configs/db');
 const { adminRouter } = require('./routes/admin.routes');
 
 const {userRouter} = require('./routes/user');
+const cors = require('cors')
 
 
 
@@ -13,6 +14,7 @@ const { logger } = require('./middlewares/logger.middleware');
 require('dotenv').config();
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 app.use(logger);
 
